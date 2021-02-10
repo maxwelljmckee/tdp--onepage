03ecfdae4a57a33b1a1079345d635f9e62023c46
maxwelljmckee <maxwelljmckee@gmail.com>
2021-02-10 11:35:46 -0700

    complete layout and styling for mission section

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Fade, Bounce } from 'react-reveal';
+import { Link } from 'react-scroll';
 
 import Header from '../Header/Header';
 import MissionCard from './MissionCard';
@@ -9,32 +10,46 @@ import missionData from './missionData';
 const Mission = () => {
   return (
     <section id='mission' className='mission__container'>
-      <Header title='mission' light />
+      <Header title='about' light />
       <div className='grid'>
-        <Bounce cascade delay={600} duration={1200}>
-          <div className='grid-left'>
-            <div className='mission__icon grid-left-1'>
-              <img src='https://crystaltechnologies.com/wp-content/uploads/2015/10/our-mission-icon.png' alt='mountains' />
+            <div className='mission__icon grid-left grid-left-1'>
+              <Bounce delay={500}>
+                <img src='https://crystaltechnologies.com/wp-content/uploads/2015/10/our-mission-icon.png' alt='mountains' />
+              </Bounce>
             </div>
-            <div className='mission__icon grid-left-2'>
-              <img src='https://motivatewellness.com/wp-content/uploads/2016/12/roadblock.png' alt='roadblock' />
+            <div className='mission__icon grid-left grid-left-2'>
+              <Bounce delay={600}>
+                <img src='https://motivatewellness.com/wp-content/uploads/2016/12/roadblock.png' alt='roadblock' />
+              </Bounce>
             </div>
-            <div className='mission__icon grid-left-3'>
-              <img src='https://www.probonoinst.org/wpps/wp-content/uploads/vote-button-circle-shape-clipart-016.jpg' alt='roadblock' />
+            <div className='mission__icon grid-left grid-left-3'>
+              <Bounce delay={700}>
+                <img src='https://www.probonoinst.org/wpps/wp-content/uploads/vote-button-circle-shape-clipart-016.jpg' alt='roadblock' />
+              </Bounce>
             </div>
-            <div className='mission__icon grid-left-4'>
-              <img src='https://pbs.twimg.com/profile_images/1219756472151601153/iKrwqrSJ_400x400.jpg' alt='roadblock' />
+            <div className='mission__icon grid-left grid-left-4'>
+              <Bounce delay={800}>
+                <img src='https://pbs.twimg.com/profile_images/1219756472151601153/iKrwqrSJ_400x400.jpg' alt='roadblock' />
+              </Bounce>
             </div>
-          </div>
-        </Bounce>
-        <Fade right delay={1000} duration={2000} cascade>
-          <div>
+
             { missionData.map(mission => {
-              return <MissionCard title={mission.title} text={mission.text} id={mission.id} />
+              return (
+                <div className='grid-right'>
+                  <Fade right delay={mission.id * 100 + 700}>
+                    
+                    <MissionCard title={mission.title} text={mission.text} 
+                      id={mission.id} />
+                  </Fade>
+                </div>
+              )
             })}
-          </div>
-        </Fade>
       </div>
+      {/* <div className='down-arrow'>
+        <Link to='demos' smooth duration={1000}>
+          <i className="fas fa-arrow-circle-down"></i>
+        </Link>
+      </div> */}
     </section>
   )
 }
