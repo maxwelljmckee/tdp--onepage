71a77a5e4917bf11b4097dacc43b3b85d57fa73b
maxwelljmckee <maxwelljmckee@gmail.com>
2021-02-11 12:17:53 -0700

    remove loader components && minor change to banner verbiage

--- src/App.js
+++ src/App.js
@@ -10,43 +10,15 @@ import TryIt from './components/TryIt/TryIt';
 import MeetDev from './components/MeetDev/MeetDev';
 
 function App() {
-  const [hide, setHide] = useState(true);
-  const [showLoader, setShowLoader] = useState(false);
-  const [showContent, setShowContent] = useState(true);
-
-  useEffect(() => {
-    // setTimeout(() => {
-    //   setHide(false);
-    //   setShowLoader(true);
-    //   setTimeout(() => {
-    //     setShowLoader(false);
-    //     setTimeout(() => {
-    //       setShowContent(true);
-    //     }, 1000)
-    //   }, 2500)
-    // }, 500)
-
-    // RESET SHOWCONTENT TO INITIALIZE TO FALSE WHEN THIS IS COMMENTED BACK IN
-  }, [])
-
   return (
     <>
-      <div className={`hidden` && hide}>
-        <Fade duration={1000} delay={500} when={showLoader} collapse>
-          <Loader />
-        </Fade>
-      </div>
-      { showContent &&
-        <>
-          <Hero />
-          <Mission />
-          <Banner text='Getting Involved Has Never Been Easier' />
-          <Demo />
-          <Banner text='Ready to try it out? Visit our mobile app!' />
-          <TryIt />
-          <MeetDev />
-        </>
-      }
+      <Hero />
+      <Mission />
+      <Banner text='Getting Involved Has Never Been Easier' />
+      <Demo />
+      <Banner text='Ready to try it out? Visit our mobile app' />
+      <TryIt />
+      <MeetDev />
     </>
   )
 }
